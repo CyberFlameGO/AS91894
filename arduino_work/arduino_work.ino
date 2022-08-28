43aba8377f9d79504b3c634efde9d297ebda53d5
CyberFlame <cyberflameu@gmail.com>
2022-08-29 10:51:28 +1200

    moto-r moto-r
    Signed-off-by: CyberFlame <cyberflameu@gmail.com>

--- arduino_work/arduino_work.ino
+++ arduino_work/arduino_work.ino
@@ -1,22 +1,37 @@
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>  // https://github.com/CyberFlameGO/Adafruit_Motor_Shield_V2_Library
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+
 /* 
  * Doesn't work, refer to the following link
  * for pin mapping as this one is incorrect.
  * https://learn.adafruit.com/adafruit-motor-shield-v2-for-arduino
  */
-int eLeft = 3;
-int mLeft = 12;
-int eRight = 11;
-int mRight = 13;
-int speed = 255;
+short eLeft = 3;
+short mLeft = 12;
+short eRight = 11;
+short mRight = 13;
+short speed = 255;
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Adafruit_DCMotor *leftMotor = AFMS.getMotor(2);
+Adafruit_DCMotor *rightMotor = AFMS.getMotor(3);
+
 
 void setup() {
-  pinMode(mLeft, OUTPUT);
-  pinMode(mRight, OUTPUT);
+//  pinMode(mLeft, OUTPUT);
+//  pinMode(mRight, OUTPUT);
+  AFMS.begin();
+  leftMotor->setSpeed(speed);
+  //leftMotor->run(FORWARD);
+  rightMotor->setSpeed(speed);
+  //rightMotor->run(FORWARD);
 }
 
 void loop() {
-  digitalWrite(mLeft, LOW);
-  digitalWrite(mRight, LOW);
+  /*
+  digitalWrite(mLeft, HIGH);
+  digitalWrite(mRight, HIGH);
   analogWrite(eLeft, speed);
   analogWrite(eRight, speed); 
+  */
 }
